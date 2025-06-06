f8521711ca6b9dd44b7c5cd8d252d9ad99f4a11e
songpeng2025 <songpeng2025@gmail.com>
2025-06-06 09:18:08 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "kk"         # 用户名
+UUID = "91fcfbf3-5b43-4e4f-880d-64afc6d6f20f"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "kk.songpeng.filegear-sg.me"                   # 域名，留空则自动获取
+CF_TOKEN = "cloudflared.exe service install eyJhIjoiZTg3ZWU0MzhlZWQwYTFjNTBlNTVhMmNkZTNlZjI0NmQiLCJ0IjoiZWYyYjM3YmYtY2MwNC00YzQ5LWEzODItYWYzYzAyNzAwZmE1IiwicyI6IlpUWmpNVGxrWkRBdE1URXdaaTAwWVdRM0xUaGhaREl0WkRsallqYzNOelF3TlRGaCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
